6b7363358120734061a9ece7ac899e5b220fc400
Martin Camitz <martin.camitz@cocoin.com>
2022-01-29 17:03:51 +0100

    Some failing tests

--- src/test/java/com/luhn/AppTest.java
+++ src/test/java/com/luhn/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void vadlidPersonnr()
+    public void validPersonnrFromUppgift()
     {
         assertTrue(ValidityCheck.isValid("201701102384"));
         assertTrue(ValidityCheck.isValid("141206-2380"));
@@ -31,5 +31,54 @@ public class AppTest
         assertTrue(ValidityCheck.isValid("900118+9811"));
         assertTrue(ValidityCheck.isValid("189102279800"));
         assertTrue(ValidityCheck.isValid("189912299816"));
+    }
+
+    @Test
+    public void badlyFormattedPnrsPass()
+    {
+        assertTrue(ValidityCheck.isValid("  201701102384  "));
+        assertTrue(ValidityCheck.isValid("141206-2380"));
+        assertTrue(ValidityCheck.isValid("20080903-2386"));
+        assertTrue(ValidityCheck.isValid("7101169295"));
+
+    }
+
+    @Test
+    public void veryBadlyFormattedPnrsDoNotPass()
+    {
+        assertFalse(ValidityCheck.isValid("20170110 2384"));
+        assertFalse(ValidityCheck.isValid("141206p2380"));
+        assertFalse(ValidityCheck.isValid("20080903- 2386"));
+        assertFalse(ValidityCheck.isValid("710116929 5"));
+
+    }
+
+    @Test
+    public void curveBallPnrsDoNotPass()
+    {
+        assertFalse(ValidityCheck.isValid("141206-2380"));
+        assertFalse(ValidityCheck.isValid("111701102384"));
+    }
+
+    @Test
+    public void inValidPersonnrFromUppgift()
+    {
+        assertFalse(ValidityCheck.isValid("201701272394"));
+        assertFalse(ValidityCheck.isValid("190302299813"));
+    }
+
+    @Test
+    public void validSamordningsNrFromUppgift()
+    {
+        assertTrue(ValidityCheck.isValid("190910799824"));
+    }
+
+    @Test
+    public void validOrgnrNrFromUppgift()
+    {
+        assertTrue(ValidityCheck.isValid("556614-3185"));
+        assertTrue(ValidityCheck.isValid("16556601-6399"));
+        assertTrue(ValidityCheck.isValid("262000-1111"));
+        assertTrue(ValidityCheck.isValid("857202-7566"));
     }
 }
