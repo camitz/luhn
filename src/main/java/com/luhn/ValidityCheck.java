30ab385a09388e2efd94a84e4def00acb61cad38
Martin Camitz <martin.camitz@cocoin.com>
2022-01-30 13:53:34 +0100

    Cleanup and comment

--- src/main/java/com/luhn/ValidityCheck.java
+++ src/main/java/com/luhn/ValidityCheck.java
@@ -6,11 +6,12 @@ import java.time.format.*;
 
 public class ValidityCheck
 {
-    private static Pattern PnrPattern = Pattern.compile("^(?<cent>(1[689]|2\\d)?)(?<year>\\d{2})(?<month>0\\d|1[012]|[2-9]\\d)(?<day>[06][1-9]|[1278]\\d|[39][01]|\\d{2})(?<sep>[-+])?(?<last>\\d{4})$");
+    //All number types are checked against this pattern.
+    private static Pattern NumberPattern = Pattern.compile("^(?<cent>(1[689]|2\\d)?)(?<year>\\d{2})(?<month>0\\d|1[012]|[2-9]\\d)(?<day>[06][1-9]|[1278]\\d|[39][01]|\\d{2})(?<sep>[-+])?(?<last>\\d{4})$");
 
     public static Result check(String number)
     {
-        var matcher = PnrPattern.matcher(number.trim());
+        var matcher = NumberPattern.matcher(number.trim());
 
         if(!matcher.find())
             return Result.Invalid;
@@ -19,24 +20,27 @@ public class ValidityCheck
         var day = matcher.group("day");
         var month = matcher.group("month");
 
-        if(!LuhnCheck(matcher.group("year") + month + day + matcher.group("last")))
+        if(!LuhnCheck(matcher.group("year") + month + day + matcher.group("last"))) //Luhn check must pass for all number types.
             return Result.Invalid;
         
-        if(month.compareTo("20") >= 0) {
+        if(month.compareTo("20") >= 0) { //Orgnr branch ends here.
             if(!(cent.isEmpty() || cent.compareTo("16") == 0))
                 return Result.Invalid;
 
             return Result.ValidOrganisationsnummer;
         }
 
-        if(day.compareTo("61") >= 0) 
+        if(day.compareTo("61") >= 0) //Samordnings number must pass date validation provided day-part reduced by 60.
             day = (Integer.parseInt(matcher.group("day")) - 60) + "";
 
-        if(matcher.group("sep") != null && matcher.group("sep").compareTo("+") == 0) {
-            if(!DateValidator.isValid(cent + matcher.group("year") + matcher.group("month") + day, true))
+        var dateStr = cent + matcher.group("year") + matcher.group("month") + day;
+        
+        //For extremely specific cases (leap day cases), the date validation is different for centenaries desgnated with + separator.
+        if(matcher.group("sep") != null && matcher.group("sep").compareTo("+") == 0) { 
+            if(!DateValidator.isValid(dateStr, true))
                 return Result.Invalid;
         }
-        else if(!DateValidator.isValid(cent + matcher.group("year") + matcher.group("month") + day, false))
+        else if(!DateValidator.isValid(dateStr, false))
             return Result.Invalid;
 
         if(Integer.parseInt(matcher.group("day")) >= 61)
@@ -66,16 +70,17 @@ public class ValidityCheck
 
     public static class DateValidator {        
         public static boolean isValid(String dateStr, boolean centenary) {
+            //Reference date is used for unsepecified century and  depends on +-separator. 
             String refDate = DateTimeFormatter.BASIC_ISO_DATE.format(java.time.LocalDate.now()); 
             if(centenary)
                 refDate = DateTimeFormatter.BASIC_ISO_DATE.format(java.time.LocalDate.now().plusYears(-100)); 
 
-            if(dateStr.length()<8) {
+            if(dateStr.length()<8) { //Century is not specified and should be added
                 dateStr = refDate.substring(0,2)+dateStr;
 
-                if(dateStr.compareTo(refDate)>0)
+                if(dateStr.compareTo(refDate)>0) //The date is later then refdate, so move back a century. Note from the spec follows that future pnrs are undefined.
                     dateStr = (Integer.parseInt(refDate.substring(0,2))-1) + dateStr.substring(2);
-            } else if(centenary && dateStr.compareTo(refDate) > 0)
+            } else if(centenary && dateStr.compareTo(refDate) > 0) //Numbers such as 19900118+9811 are invalid (younger than 100 with +-separator).
                 return false;
 
             try {
